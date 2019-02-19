41dc5afaeeb38c9f2df2243784d5f00ef0a499dc
Sebastian Nilsson <sebbehebbe@gmail.com>
2019-02-19 11:40:38 +0100

    Programming complete

--- problem2.m
+++ problem2.m
@@ -1,11 +1,13 @@
-data_raw = readtable('../data/whales.txt', 'ReadVariableNames', false);
+data_raw = readtable('./whales.txt', 'ReadVariableNames', false);
 data = data_raw.Var1;
-N = length(data); %% Number of counties
+n = length(data); %% Number of counties
 
-%% Task a
+%% Task a'
+bin_width = 0.28;
+figure(1);
 clf;
-histogram(data, 20);
-hold on
+h = histogram(data, 20, 'BinWidth', bin_width);
+hold on;
 
 % Task b
 
@@ -14,9 +16,80 @@ mu1 = mean(data);
 mu2 = mean(data.^2);
 
 % Create alhpa and lambda estimates from moments
-l_hat = mu1/(mu2-mu1^2);    
-a_hat = l_hat*mu1;
+a_hat_mme = mu1^2/(mu2-mu1^2);
+l_hat_mme = a_hat_mme/mu1;
 
-gam = gampdf(0:0.01:5, a_hat,l_hat);
-plot(0:0.01:5, gam*60);
-legend('Real Data', sprintf('Gam(%0.2f, %0.2f)', a_hat,l_hat))
+gam = gampdf(0:0.01:max(data), a_hat_mme,l_hat_mme);
+plot(0:0.01:max(data), gam*n*bin_width, 'LineWidth',1);
+
+%% Task c
+% Mostlikelihood gives us a_hat_mle = 1.5955
+% The a_hat_mle can be found with function gamfit, but I found it using
+% mathematica
+a_hat_mle = 1.5954;
+l_hat_mle = mean(data)/a_hat_mle;
+
+%% Task d
+gam_mle = gampdf(0:0.01:max(data), a_hat_mle,0.37);
+plot(0:0.01:max(data), gam_mle*n*bin_width, 'LineWidth',1);
+legend('Real Data', sprintf('Gam(%0.2f, %0.2f) MME', a_hat_mme,l_hat_mme), sprintf('Gam(%0.2f, %0.2f) MLE', a_hat_mle,l_hat_mle))
+
+%% Task e
+% Parametric bootstrap
+N = 1500;
+n = 200;
+
+mme_gamma_dist_draws = random('Gamma', a_hat_mme, l_hat_mme, [N n]);
+mu1_mme_bootstrap = mean(mme_gamma_dist_draws, 2);
+mu2_mme_bootstrap = mean(mme_gamma_dist_draws .^2, 2);
+alphas = (mu1_mme_bootstrap.^2)./(mu2_mme_bootstrap - mu1_mme_bootstrap.^2);
+lambdas = alphas ./ mu1_mme_bootstrap;
+
+figure(2);
+clf;
+histogram(alphas, 20)
+title('MME Alpha Sampling Distribution')
+figure(3);
+clf;
+histogram(lambdas, 20)
+title('MME Lambda Sampling Distribution')
+
+fprintf('Standard error for MME alpha sampling distribution: %0.4f\n', std(alphas));
+fprintf('Standard error for MME lambda sampling distribution: %0.4f\n', std(lambdas))
+
+%% Task f
+N = 1500;
+n = 200;
+
+mle_lambas_alphas = zeros(N,2);
+mle_gamma_dist_draws = random('Gamma', a_hat_mle, l_hat_mle, [N n]);
+for i = 1:N
+    sample = mle_gamma_dist_draws(i,:);
+    mle_lambas_alphas(i,:) = gamfit(sample);
+end
+% Plot alpha sampling distribution
+figure(4);
+histogram(mle_lambas_alphas(:,1),20)
+title('MLE Alpha Sampling Distribution')
+
+figure(5);
+% Plot lambda sampling distribution
+histogram(mle_lambas_alphas(:,2),20)
+title('MLE Lambda Sampling Distribution')
+
+mle_bootstrap_alpha_std = std(mle_lambas_alphas(:,1));
+mle_bootstrap_lambda_std = std(mle_lambas_alphas(:,2));
+
+fprintf('Standard error for MLE alpha sampling distribution: %0.4f\n', mle_bootstrap_alpha_std);
+fprintf('Standard error for MLE lambda sampling distribution: %0.4f\n', mle_bootstrap_lambda_std)
+
+%% g
+mle_params_sorted = sort(mle_lambas_alphas);
+alpha_delta_lower = mle_params_sorted(round(N*0.05),1) - a_hat_mle;
+alpha_delta_upper = mle_params_sorted(round(N*0.95),1) - a_hat_mle;
+
+lambda_delta_lower = mle_params_sorted(round(N*0.05),2) - l_hat_mle;
+lambda_delta_upper = mle_params_sorted(round(N*0.95),2) - l_hat_mle;
+
+mle_alpha_interval = [a_hat_mle - alpha_delta_upper, a_hat_mle - alpha_delta_lower];
+mle_lambda_interval = [l_hat_mle - lambda_delta_upper, l_hat_mle - lambda_delta_lower];
